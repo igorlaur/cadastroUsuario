588190d4784252e599efbaf519a1005e0ce3e415
laur <igor.laur2@gmail.com>
2018-12-31 17:11:00 -0200

    Initial Commit

--- crud/frontend/src/components/template/Header.jsx
+++ crud/frontend/src/components/template/Header.jsx
@@ -2,6 +2,9 @@ import './Header.css'
 import React from 'react'
 
 export default props =>
-    <header className="header">
-        Cabeçalho
+    <header className="header d-none d-sm-flex flex-column"> {/* d-nome = para celulares o header não aparecerá. d-sm-flex para dispositivos small para cima ele vai usar display flex */}
+        <h1 className="mt-3"> {/* Margem top 3 */}
+            <i className={`fa fa-${props.icon}`}></i> {props.title} {/* fa fa = classe do font-awesome para aparecer a casinha */}
+        </h1>
+        <p className="lead text-muted">{props.subtitle}</p>
     </header>
--- crud/frontend/src/components/template/Main.jsx
+++ crud/frontend/src/components/template/Main.jsx
@@ -4,7 +4,7 @@ import Header from './Header'
 
 export default props =>
     <React.Fragment> {/*Como não é possível importar dois componentes JSX em uma div, então utilizamos React.Fragment*/}
-        <Header /> 
+        <Header {...props} /> {/* props = As propriedades que eu recebi no Main.jsx estão sendo propagadas no Header. Recebendo Ícone, título e subtítulo */} 
             <main className="content">
                 Conteúdo
             </main>
