aa67a2fac7f7f157ec921c7d217876bb6565f28a
laur <igor.laur2@gmail.com>
2018-12-31 20:33:14 -0200

    Initial Commit

--- crud/frontend/src/components/template/Main.jsx
+++ crud/frontend/src/components/template/Main.jsx
@@ -5,7 +5,9 @@ import Header from './Header'
 export default props =>
     <React.Fragment> {/*Como não é possível importar dois componentes JSX em uma div, então utilizamos React.Fragment*/}
         <Header {...props} /> {/* props = As propriedades que eu recebi no Main.jsx estão sendo propagadas no Header. Recebendo Ícone, título e subtítulo */} 
-            <main className="content">
-                Conteúdo
+            <main className="content container-fluid"> {/* Container-fluid adicionado porque vou usar padrão de 12 colunas etc*/}
+                <div className="p-3 mt-3"> {/* Paddin: 3 && Margem-top: 3 */}
+                    {props.children} {/* Componentes filho */}
+                </div>
             </main>
     </React.Fragment>
